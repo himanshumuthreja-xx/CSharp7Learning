ce443106de41648a08411a16a9fd017b62ca43d9
Himanshu Muthreja <hmuthreja7@gmail.com>
2018-01-28 22:47:25 +0530

    LocalMethods

---
+++ LocalMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LocalMethods
+{
+    class Program
+    {
+        public string name;
+        public Program(string n) => name = n; 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Writing cubes using local methods!");
+            WriteCubes();
+            Console.ReadLine();
+        }
+
+        static void WriteCubes()
+        {
+            Console.WriteLine($"Cube of 3 is {Cube(3)}");
+            Console.WriteLine($"Cube of 4 is {Cube(4)}");
+            Console.WriteLine($"Cube of 5 is {Cube(5)}");
+            int Cube(int number) => number * number * number;
+        }
+    }
+}
